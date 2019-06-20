bd1178cb308cd3c2129fcd11dcfc7084bfa771ad
Cybdom <amazighhalzoun@gmail.com>
2019-06-20 23:27:07 +0100

    Done

--- lib/main.dart
+++ lib/main.dart
@@ -3,109 +3,382 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primaryColor: Color(0xfff5f7f9),
+        accentColor: Colors.white,
+        textTheme: TextTheme(
+          title: TextStyle(
+            fontSize: 35,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+          body1: TextStyle(
+            fontSize: 15,
+            color: Color(0xffb2b5be),
+          ),
+        ),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.white,
+          leading: IconButton(
+            icon: Icon(
+              Icons.menu,
+              color: Colors.black,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            onPressed: () {},
+          ),
+          actions: <Widget>[
+            Container(
+              width: 50,
+              margin: EdgeInsets.only(right: 9.0),
+              child: CircleAvatar(
+                backgroundImage: NetworkImage(
+                  "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=format%2Ccompress&cs=tinysrgb&dpr=1&w=500",
+                ),
+              ),
+            )
+          ],
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(
+                    color: Colors.grey[300],
+                  ),
+                ),
+                color: Colors.white,
+              ),
+              padding: EdgeInsets.only(left: 15.0, bottom: 15.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    "Hi, Amazigh Halzoun",
+                    style: Theme.of(context).textTheme.title,
+                  ),
+                  Text("Tuesday, Jun 20"),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 21,
+            ),
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.all(13.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Text("UPCOMMING APPOINTMENTS"),
+                    Container(
+                      height: 200,
+                      child: ListView(
+                        scrollDirection: Axis.horizontal,
+                        children: <Widget>[
+                          AppointmentCard(),
+                          AppointmentCard(),
+                          AppointmentCard(),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 9,
+                    ),
+                    Text("My MEDICINES"),
+                    SizedBox(
+                      height: 9,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: MedecineContainer(
+                            active: true,
+                            time: "Morning",
+                          ),
+                        ),
+                        SizedBox(
+                          width: 11,
+                        ),
+                        Expanded(
+                          child: MedecineContainer(
+                            active: false,
+                            time: "Afternoon",
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 11.0,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: MedecineContainer(
+                            active: false,
+                            time: "Evening",
+                          ),
+                        ),
+                        SizedBox(
+                          width: 11,
+                        ),
+                        Expanded(
+                          child: MedecineContainer(
+                            active: false,
+                            time: "Night",
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 9,
+                    ),
+                    Text("MESSAGES"),
+                    SizedBox(
+                      height: 9,
+                    ),
+                    Expanded(
+                      child: ListView(
+                        children: <Widget>[
+                          MessagesCard(
+                            color: Colors.blue,
+                            icon: Icon(
+                              Icons.calendar_today,
+                              color: Colors.white,
+                            ),
+                          ),
+                          MessagesCard(
+                            color: Colors.blue,
+                            icon: Icon(
+                              Icons.calendar_today,
+                              color: Colors.white,
+                            ),
+                          ),
+                          MessagesCard(
+                            color: Colors.blue,
+                            icon: Icon(
+                              Icons.calendar_today,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class AppointmentCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(11.0),
+      padding: EdgeInsets.all(15.0),
+      width: 250,
+      height: 190,
+      decoration: BoxDecoration(
+        color: Colors.white,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text("Consulting Doctor"),
+          SizedBox(
+            height: 21,
+          ),
+          FittedBox(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                CircleAvatar(
+                  backgroundImage: NetworkImage(
+                    "http://pngimg.com/uploads/doctor/doctor_PNG15959.png",
+                  ),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      "Anthony Morrety",
+                      style: TextStyle(fontSize: 23, color: Colors.blueAccent),
+                    ),
+                    Text(
+                      "Clinical Medicine",
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Spacer(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text(
+                "Tommorow",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                "03:30PM",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class MedecineContainer extends StatelessWidget {
+  final String time;
+  final bool active;
+  const MedecineContainer({Key key, this.time, this.active}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(21),
+      decoration: BoxDecoration(
+        color: active ? Colors.blue : Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey[300],
+            blurRadius: 3.0,
+            offset: Offset(0, 1),
+          ),
+        ],
+      ),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Text(
+              time,
+              style: TextStyle(
+                  color: active ? Colors.white : Colors.black,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 11,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Icon(
+                  Icons.block,
+                  color: Colors.yellow,
+                ),
+                active
+                    ? Container()
+                    : Icon(
+                        Icons.block,
+                        color: Colors.blue,
+                      ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MessagesCard extends StatelessWidget {
+  final Color color;
+  final Icon icon;
+  const MessagesCard({Key key, this.color, this.icon}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [BoxShadow(color: Colors.grey[300], blurRadius: 3.0, offset: Offset(0,1))]
+      ),
+      margin: EdgeInsets.symmetric(vertical: 11.0, horizontal: 5.0),
+      padding: EdgeInsets.all(11.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(5.0),
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: color,
+            ),
+            child: icon,
+          ),
+          SizedBox(
+            width: 21,
+          ),
+          Expanded(
+            child: Column(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Text(
+                      "Appointment Reminder",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.black87),
+                    ),
+                    Spacer(),
+                    Text("10:00"),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Text(
+                      "You have an appointment with Amazigh Halzoun",
+                      overflow: TextOverflow.ellipsis,
+                      softWrap: false,
+                    ),
+                    Spacer(),
+                    Material(
+                      color: Colors.transparent,
+                      child: IconButton(
+                        icon: Icon(Icons.chevron_right),
+                        onPressed: () {},
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
